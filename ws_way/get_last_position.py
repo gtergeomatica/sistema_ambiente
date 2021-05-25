2821eef92b61ed7c208cb01a7f15c349d070ef81
amrossx <ambrosino.rossella@gmail.com>
2021-05-25 15:33:32 +0200

    completato script con insert su db

--- ws_way/get_last_position.py
+++ ws_way/get_last_position.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # Gter copyleft 2021
-# Author: Roberto Marzocchi
+# Author: Roberto Marzocchi - Rossella Ambrosino
 # Script che interroga i WS di WAY srl per ottenere le posizioni dei mezzi
+# lo script è a crontab
+# * * * * * gter /usr/bin/python3 /home/gter/REPOSITORY/sistema_ambiente/ws_way/get_last_position.py > /dev/null 2>&1
 
 
 
@@ -13,21 +15,18 @@
 import os, sys, re  # ,shutil,glob
 import numpy as np
 import getopt  # per gestire gli input
-
-#import pymssql
-
 import psycopg2
+import requests
+import token_api as t
+import xml.etree.ElementTree as et
+from xml.etree.ElementTree import parse
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from credenziali import db, port, user, pwd, host
 
-
-import requests
-
 import logging
-
 path=os.path.realpath(__file__).replace('get_last_position.py', '')
 #tmpfolder=tempfile.gettempdir() # get the current temporary directory
 logfile='{}/log/get_last_position.log'.format(path)
@@ -37,14 +36,10 @@ logfile='{}/log/get_last_position.log'.format(path)
 logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s',
     filemode='a', # overwrite or append
     filename=logfile,
-    #level=logging.WARNING)
-    level=logging.DEBUG)
+    level=logging.INFO)
+    #level=logging.DEBUG)
 
-import token_api as t
 
-import xml.etree.ElementTree as et
-
-from xml.etree.ElementTree import parse
 
 
 def GetVehicleLastEvent(token):
@@ -67,26 +62,28 @@ def GetVehicleLastEvent(token):
     </mob:GetVehicleLastEvent>
     </soap:Body>
     </soap:Envelope>""".format(token)
+
     r = requests.post(url, data=body1.encode(encoding='utf-8'), headers=headers)
     logging.debug("Info recieved...")
-    logging.debug(r.content)
+    #logging.debug(r.content)
     ret= r.status_code
     if ret == 200: 
         logging.debug("Vehicle Last position obtained")
-    logging.debug(r.content)
+    
     root = et.fromstring(r.content)
-    logging.debug(root)
+    #logging.debug(root)
     n_Vehicle=len(root[0][0][0][5].tag)
     i=0
 
     result_list = []
     for v in root[0][0][0][5]:
+
         #itero sui veicoli
         vdict = {}
 
         for c in v:
-            #itero sui tag di ogni veicolo
 
+            #itero sui tag di ogni veicolo
             key = c.tag.split('}')[-1].strip().lower()
             if c.text != None:           
                 value = c.text.strip().lower()
@@ -114,13 +111,15 @@ def GetVehicleHistoryEvents(token):
 
 def main():
     # recupero il token 
-    logging.debug('*'*20 + 'NUOVA ESECUZIONE' + '*'*20)
-    r= requests.get('{}/GetToken'.format(t.url), params={'serverName':t.servername, 'userName':t.username, 'password':t.password})
+    logging.info('*'*20 + 'NUOVA ESECUZIONE' + '*'*20)
+    r= requests.get('{}/GetToken'.format(t.url), params={'serverName':t.servername,
+                                                         'userName':t.username, 
+                                                         'password':t.password})
     logging.debug(r.status_code)
     logging.debug(r.url)
     root = et.fromstring(r.content)
     token=root.text
-    print(token)
+    logging.debug(token)
     
     
     logging.debug('GO!')
@@ -132,72 +131,98 @@ def main():
         os._exit(1)
 
 
-       # carico i mezzi sul DB PostgreSQL
+    # carico la risposta sul DB PostgreSQL
     logging.info('Connessione al db')
-    conn = psycopg2.connect(dbname=db,
+    try:
+        conn = psycopg2.connect(dbname=db,
                         port=port,
                         user=user,
                         password=pwd,
                         host=host)
-
+    except:
+        logging.error('connessione al db fallita ')
+        logging.exception('')
+        os._exit(1)   
+    
+    
     curr = conn.cursor()
     conn.autocommit = True
     
-    n_mezzi= len(risposta) #326
-    column_list = [i for i in risposta[0]]
-    
-    print(column_list)
-    
 
-
+    
     #create table query
-    create_table_query = ''' CREATE TABLE IF NOT EXISTS ws_way.t_last_position (
-                            id SERIAL PRIMARY KEY,
-                            id_mezzo varchar,
-                            nome varchar,
-                            targa varchar,
-                            brand varchar,
-                            modello varchar)  '''
+    create_table_query =  '''CREATE TABLE IF NOT EXISTS ws_way.t_last_position (
+                            id_row SERIAL PRIMARY KEY,
+                            date_ins timestamp DEFAULT NOW(),
+                            id varchar UNIQUE,
+                            idvehicle varchar,
+                            name varchar, 
+                            plate varchar,
+                            vin varchar , 
+                            eventdate timestamp,
+                            eventid varchar,
+                            eventcode varchar , 
+                            eventdescription varchar,
+                            realtime boolean, 
+                            positiondate timestamp,
+                            latitude numeric,
+                            longitude numeric,
+                            altitude numeric,
+                            direction numeric,
+                            speed numeric,
+                            km numeric,
+                            kmcanbus varchar ,
+                            workhour numeric ,
+                            idfleet varchar,
+                            idstate varchar,
+                            vehiclesensors varchar,
+                            address varchar)  '''
 
     
     try:
 	    curr.execute(create_table_query)
 	    conn.commit()
-	    logging.info(''' Creata tabella dei mezzi''')
+	    logging.debug(''' Verificata presenza tabella ultime posizioni''')
     except Exception as e:
-	    logging.error(e)                            
+	    logging.error(e)
+                         
     
     #insert table query
     #organizzo i dati per l'insert
-    
-    records_list_template = ','.join(['%s'] *n_mezzi )
-    a = np.array([['ND' if i is None else i for i in risposta[0]],
-                 ['ND' if i is None else i for i in risposta[1]],
-                 ['ND' if i is None else i for i in risposta[2]],
-                ['ND' if i is None else i for i in risposta[3]],
-                ['ND' if i is None else i for i in risposta[4]]])    
-    
-    b = np.transpose(a)
-    c = b.tolist()
-    d = [tuple(i) for i in c]
+    n_mezzi= len(risposta) #326
+    column_list = [i for i in risposta[0]]
+    #column_type = {i: type(i) for i in risposta[0]} 
+    #inutile: in lettura del ws tutti i valori sono stringhe
 
-    
-    insert_query = '''INSERT INTO ws_way.t_mezzi (id_mezzo, nome,targa, brand, modello) values {}'''.format(records_list_template)
+     
+    records_list_template = ', '.join(['%s'] *n_mezzi )
+    column_list_template = ', '.join(column_list )
 
+    #scrivo le liste di valori da inserire secondo l'ordine definito in column_lisr
+    rowstoinsert = [tuple([i[c] for c in column_list ]) for i in risposta]
+
+    #in caso di id già presente non viene effettuato nessun insert
+    insert_query = '''INSERT INTO ws_way.t_last_position ({})
+                   values {} 
+                   ON CONFLICT (id) DO NOTHING;'''.format(column_list_template,
+                                                          records_list_template)
+
+    logging.debug(insert_query)
     #con la funzione mogrify metto in successione la query scritta prima con l'array
-    query_insert2=curr.mogrify(insert_query, d).decode('utf8')
-
+    query_insert2=curr.mogrify(insert_query, rowstoinsert ).decode('utf8')
+    
     # lancio la query dentro un try per segnalare eventuali errori
     try:
 	    curr.execute(query_insert2)
 	    conn.commit()
-	    logging.info('''INSERT MEZZI CONCLUSO''')
+	    logging.info('''Insert concluso con successo''')
     except Exception as e:
-	    logging.error(e)
+        logging.error('Insert fallito {}'.format(insert_query))
+        logging.error(e)
+        os._exit(1)
 
 
-
-    logging.debug('*'*20 +'ESCO NORMALMENTE' + '*'*20 )
+    logging.info('*'*20 +'ESCO NORMALMENTE' + '*'*20 )
 
 if __name__ == "__main__":
     main()
