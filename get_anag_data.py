e957f82dd012c0df92b27abc56ae00e9f9396f29
amrossx <ambrosino.rossella@gmail.com>
2021-10-11 16:00:24 +0200

    modificata gestione upload

--- get_anag_data.py
+++ get_anag_data.py
@@ -3,10 +3,10 @@
 '''
 
 Script per la sincronizzazione giornaliera
-dei dati delle anagrafiche contenute nella banca dati Garbage
+dei dati delle anagrafiche2 contenute nella banca dati Garbage
 
 
-il file viene normalmente generato a mezzanotte (12:00 AM)
+il file  di Garbage viene normalmente generato a mezzanotte (12:00 AM)
 
 
 
@@ -22,7 +22,7 @@ from datetime import datetime
 import psycopg2
 import logging
 import pandas as pd
-
+from sqlalchemy import create_engine
 from credenziali import *
 
 # recupero il percorso allo script python 
@@ -37,16 +37,10 @@ logging.basicConfig(
 
 logging.info('*'*20 + 'NUOVA ESECUZIONE' + '*'*20)
 
-
-
-#session = FTP(ftp_host, ftp_user, ftp_pwd)
 filename='{}/log/anag_data.csv'.format(spath) #, datetime.now().strftime("%Y%m%d_%H%M%S"))
 try:
     with FTP(ftp_host, ftp_user, ftp_pwd) as ftp:
-        #modtime = ftp.sendcmd('MDTM ' + anag_filename)
-        #modtime2 = datetime.strptime(modtime[4:], "%Y%m%d%H%M%S").strftime("%Y%m%d_%H%M%S") #.strftime("%d %B %Y %H:%M:%S")
-        logging.info('{}'.format(ftp.dir()))
-        #filename='{}/log/anag_data_{}.csv'.format(spath, modtime2) 
+        #logging.info('{}'.format(ftp.dir()))
         with open(filename, 'wb') as downloaded_file:
             ftp.retrbinary('RETR %s' % anag_filename, downloaded_file.write)
 except:
@@ -54,50 +48,45 @@ except:
     logging.exception('')
     os._exit(1)
     
-'''
-
-Index(['CodiceUtenza', 'CodVia', 'Toponimo', 'Denominazione', 'datafine',
-       'NumeroCivico', 'Lettera', 'Localita', 'TipoUtenza', 'Descrizione',
-       'MQLocali', 'NumComponenti', 'cond', 'CodiceContribuente',
-       'Denominazione1', 'Denominazione2'],
-      dtype='object')
-'''
-
-#df = pd.read_csv(filename, sep=';', encoding='iso8859')
-#df.head().to_csv('{}/log/test.csv'.format(spath), sep=';')
 
 
-table_create_sql = ''' CREATE TABLE IF NOT EXISTS anagrafiche.anag_garbage (
-                     id serial primary key,
-                    'CodiceUtenza' varchar ,
-                    'CodVia' varchar,
-                    'Toponimo' varchar,
-                    'Denominazione' varchar,
-                    'datafine' timestamp without time zone,
-                    'NumeroCivico' varchar,
-                    'Lettera' varchar,
-                    'Localita' varchar,
-                    'TipoUtenza'varchar,
-                    'Descrizione'varchar,
-                    'MQLocali' integer, 
-                    'NumComponenti' integer,
-                    'cond' boolean,
-                    'CodiceContribuente' varchar,
-                    'Denominazione1' varchar,
-                    'Denominazione2' varchar
+table_create_sql = ''' CREATE TABLE IF NOT EXISTS anagrafiche2.anag_garbage (
+                     --id serial primary key,
+                    "CodiceUtenza" varchar primary key,
+                    "CodVia" varchar,
+                    "Toponimo" varchar,
+                    "Denominazione" varchar,
+                    "datafine" timestamp without time zone,
+                    "NumeroCivico" varchar,
+                    "Lettera" varchar,
+                    "Localita" varchar,
+                    "TipoUtenza" varchar,
+                    "RESIDENTE" varchar,
+                    "Descrizione" varchar,
+                    "MQLocali" integer, 
+                    "NumComponenti" integer,
+                    "cond" boolean,
+                    "CodiceContribuente" varchar,
+                    "Denominazione1" varchar,
+                    "Denominazione2" varchar,
+                    "ServizioPannoloni" boolean
                     )
                     '''
 
-truncate_sql = '''TRUNCATE TABLE anagrafiche.anag_garbage'''
+truncate_sql = '''TRUNCATE TABLE anagrafiche2.anag_garbage'''
 
-sql = ''' COPY anagrafiche.anag_garbage
-          FROM {} 
-          DELIMITER ';' CSV;
-      '''.format(filename)
+"""
+ sql = '''COPY anagrafiche2.anag_garbage
+          FROM STDIN
+          DELIMITER ';' 
+          CSV HEADER
+          QUOTE "'"
+          ENCODING 'LATIN1'; 
+      ''' 
+"""
 
 
-
-sql_list = [ table_create_sql, sql,  truncate_sql ]
+sql_list = [ table_create_sql,  truncate_sql ]
 
 logging.info('Connessione al db')
 conn = psycopg2.connect(dbname=db,
@@ -110,14 +99,46 @@ cur = conn.cursor()
 conn.autocommit = True
 
 
-for sql in sql_list:
-    logging.info(sql)
-    try:
-        cur.execute(sql)
-    except:
-        logging.error('Sql command fail')
-        logging.exception('')
-        os._exit(1)
+
+for s in sql_list:
+
+    logging.info(s)
+
+    if s.startswith('COPY'):
+
+        try:
+            cur.copy_expert(s, open(filename, "r", encoding="latin_1"))
+        except:
+            logging.error('Sql COPY command fail')
+            logging.exception('')
+            os._exit(1)           
+
+    else:
+
+        try:
+            cur.execute(s)
+        except:
+            logging.error('Sql command fail')
+            logging.exception('')
+            os._exit(1)
+
+
+conn_string = 'postgres://{}:{}@{}/{}'.format(user, pwd, host, db )
+engine = create_engine(conn_string)
+connection = engine.connect()
+
+df = pd.read_csv(filename, sep=';', encoding='iso8859', 
+                 dtype={'cond' : bool, 'ServizioPannoloni':bool,
+                       'CodVia': str,  'NumeroCivico':str},
+                 true_values=['SI'], false_values=['NO'])
+#df.head().to_csv('{}/log/anag_garbage_sample.csv'.format(spath), sep=';')
+
+try:
+    df.to_sql('anag_garbage', con=connection, schema='anagrafiche2', if_exists='append', index=False)
+except:
+    logging.error('Sql command fail')
+    logging.exception('')
+    os._exit(1)
 
 
 logging.info('*'*20 + 'FINE ESECUZIONE' + '*'*20)  
