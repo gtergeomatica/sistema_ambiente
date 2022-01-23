be0894bdbd16e24cbd52b2067c138dd21d9616e9
Francesco Bursi <francesco.bursi@hotmail.it>
2022-01-24 00:00:33 +0100

    update

--- get_rfid_data.py
+++ get_rfid_data.py
@@ -7,6 +7,7 @@ dei dati dei tag rfid dei cestini
 
 
 """
+
 from ftplib import FTP
 import os
 import sys
@@ -18,37 +19,86 @@ from datetime import datetime
 import psycopg2
 import logging
 import pandas as pd
+import csv
+import pathlib
+
 from sqlalchemy import create_engine
 from credenziali import *
 
-def main():
-    # recupero il percorso allo script python
+
+def initLogger():
+    """
+    define log file as simple as possibile
+    """
+
     spath = os.path.dirname(os.path.realpath(__file__))
 
     logging.basicConfig(
         format="%(asctime)s\t%(levelname)s\t%(message)s",
-        filemode="w",  # overwrite or append
-        # filename='{}/log/get_rfid.log'.format(spath),
+        filemode="a",  # overwrite or append
+        filename=f"{spath}/log/get_rfid.log",
         level=logging.INFO,
     )
 
     logging.info("*" * 20 + "NUOVA ESECUZIONE" + "*" * 20)
 
-    filename = "{}/log/rfid_data.csv".format(
-        spath
-    )  # , datetime.now().strftime("%Y%m%d_%H%M%S"))
+
+def getFileList(ftp):
+    """
+    list of csv file in the folder
+    """
+
+    files = []
 
     try:
+        files = ftp.nlst()
+    except ftplib.error_perm as resp:
+        if str(resp) == "550 No files found":
+            logging.error("No files in this directory")
+        else:
+            raise
+    return files
+
+
+def csvGrabber(listFile, ftp):
+    """
+    download files from ftp folder to actual folder if they are csv
+    """
+    for file in listFile:
+        with open(file, "wb") as downloadedFile:
+            ftp.retrbinary(f"RETR {file}", downloadedFile.write)
+            logging.info(f"{file} è stato scaricato")
+
+        downloadedFile.close()
+
+
+def parser():
+    pass
+
+
+def main():
+    initLogger()
+    try:
         with FTP(ftp_host_g, ftp_user_g_01, ftp_pwd_g_01) as ftp:
-            logging.info("{}".format(ftp.dir()))
-            # with open(filename, 'wb') as downloaded_file:
-            # ftp.retrbinary('RETR %s' % anag_filename, downloaded_file.write)
+
+            dir_list = []
+            ftp.dir(dir_list.append)
+            logging.info(f"lista di file presenti {dir_list}")
+
+            fileList = getFileList(ftp)
+
+            filename = "letture_bracciale.csv"
+            csvGrabber(fileList, ftp)
+
+            ftp.quit()
+
     except:
         logging.error("Trasferimento non riuscito")
         logging.exception("")
         os._exit(1)
-        
+
     logging.info("*" * 20 + "FINE ESECUZIONE" + "*" * 20)
-    
+
+
 if __name__ == "__main__":
     main()
